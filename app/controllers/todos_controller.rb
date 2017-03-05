c94d1bceaac0a2dd308f9159530daafb7f06eeb6
Paul Denlinger <pmdenlinger@icloud.com>
2017-03-05 23:34:11 +0000

    added dependencies and refactored code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:edit, :update, :show, :destroy]
   
   def new
     @todo = Todo.new
@@ -15,40 +16,40 @@ class TodosController < ApplicationController
   end
     
   def show
-    @todo = Todo.find(params[:id])
   end
   
   def edit
-    @todo = Todo.find(params[:id])
   end
   
   def update
-    @todo = Todo.find(params[:id])
-    if @todo.update(todo_params)
+   if @todo.update(todo_params)
       flash[:notice] = "Todo was successfully updated"
       redirect_to todo_path(@todo)
     else
       render 'edit'
     end
    end
+ 
     
     def index
       @todos = Todo.all
     end
     
     def destroy
-      @todo = Todo.find(params[:id])
       @todo.destroy
       flash[:notice] = "Todo was deleted successfully"
       redirect_to todos_path
     end
     
- 
-  
   private
+ 
+    def set_todo
+       @todo = Todo.find(params[:id])
+    end
   
     def todo_params
       params.require(:todo).permit(:name, :description)
     end
   end
+  
 
